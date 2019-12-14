7bc48b4a5f2e2a0e4a3cac7585946d5f7e9c74f6
know112-state <jianyu85200@126.com>
2019-12-14 22:35:36 +0800

    first commit

---
+++ server.js
@@ -0,0 +1,9 @@
+/*jshint esversion: 6 */
+const express = require("express");
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+    console.log(`服务器启动成功:${port}`);
+})
