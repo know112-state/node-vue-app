754ce0fb94ef2fb11674b377b0102b3094d57c2d
know112-state <jianyu85200@126.com>
2019-12-14 22:57:54 +0800

    小测试

--- server.js
+++ server.js
@@ -2,8 +2,12 @@
 const express = require("express");
 const app = express();
 
+app.get("/", (req, res) => {
+    res.send("Hello world!");
+});
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
     console.log(`服务器启动成功:${port}`);
-})
+});
